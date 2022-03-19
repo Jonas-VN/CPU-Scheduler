f9b418465f3471023e891529a2cb63ab1dbf2701
Jonas-VN <87768347+Jonas-VN@users.noreply.github.com>
2022-03-19 23:18:30 +0100

    Update FCFS.py

--- FCFS.py
+++ FCFS.py
@@ -1,5 +1,6 @@
 import xml.etree.ElementTree as ET
-import matplotlib
+from matplotlib import pyplot as plt
+import numpy as np
 
 def parse_data(filename):
     tree = ET.parse("./Data/" + filename)
@@ -12,17 +13,15 @@ def parse_data(filename):
         data.append([int(process[1].text), int(process[2].text)])
     return data
 
-
 data = parse_data("processen50000.xml")
 jiffy = 0  # 1 jiffy is 1 CPU cycle
-finished = False
 scheduled = [
     # [arrival time, service time, waiting time, turnaround time, response ratio]
 ]
 index = 0
 aantal_processen = len(data)
 
-while not finished:
+while True:
     if index == aantal_processen:
         break
 
@@ -37,12 +36,13 @@ while not finished:
         scheduled.append([arrival_time, service_time, waiting_time, turnaround_time, response_ratio])
         index += 1
         jiffy += service_time
-
-    jiffy += 1
+    else:
+        jiffy += 1
 
 gemiddelde_turnaround_time = 0
 gemiddelde_waiting_time = 0
 gemiddelde_response_ratio = 0
+
 for i in scheduled:
     gemiddelde_waiting_time += i[2]
     gemiddelde_turnaround_time += i[3]
@@ -55,3 +55,14 @@ gemiddelde_response_ratio /= aantal_processen
 print("Gemiddelde waiting_time: " + str(gemiddelde_waiting_time))
 print("Gemiddelde turnaround_time: " + str(gemiddelde_turnaround_time))
 print("Gemiddelde response_ratio: " + str(gemiddelde_response_ratio))
+
+response_ratios = np.array([i[3] for i in scheduled])
+waiting_times = np.array([i[1] for i in scheduled])
+x = [i for i in range(0, 101, 10)]
+
+y = np.percentile(response_ratios, x)
+z = np.percentile(waiting_times, x)
+
+#plt.plot(x, y)
+plt.plot(x, z)
+plt.show()
