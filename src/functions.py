20c45e5011d108d980e0aabfb93225d387f16911
Jonas-VN <87768347+Jonas-VN@users.noreply.github.com>
2022-04-03 08:32:33 +0200

    Update functions.py

--- src/functions.py
+++ src/functions.py
@@ -1,5 +1,4 @@
 import numpy as np
-from matplotlib import pyplot as plt
 import xml.etree.ElementTree as ET
 
 
@@ -15,11 +14,11 @@ def parse_data(filename):
     return data
 
 
-def visualize_data(scheduled):
+def calc_percentile(scheduled):
     scheduled = sorted(scheduled, key=lambda x: x[2])  # Sorteren op sevice time
     response_ratios = np.array([i[5] for i in scheduled])  # De response ratios die al gesorteerd zijn op service time
     waiting_times = np.array([i[3] for i in scheduled])  # De waiting times      "  "     "        "   "    "      "
-    x = [i for i in range(1, 101)]  # array van 1 -> 100 om alle percentielen te berekenen en als x-as op de grafiek
+    x = range(1, 101)  # array van 1 -> 100 om alle percentielen te berekenen en als x-as op de grafiek
     step = len(scheduled) // len(x)  # stap per percentiel, voor 10_000 processen: 10_000 // 100 = 100 processen per percentiel
 
     gemiddelde_response_ratios_per_percentiel = []
@@ -34,9 +33,7 @@ def visualize_data(scheduled):
         gemiddelde_response_ratios_per_percentiel.append(sum(response_ratios_per_percentiel) / len(response_ratios_per_percentiel))  # gemiddelde toevoegen
         gemiddelde_waiting_times_per_percentiel.append(sum(waiting_times_per_percentiel) / len(waiting_times_per_percentiel))  # gemiddelde toevoegen
 
-    plt.plot(x, gemiddelde_response_ratios_per_percentiel)
-    plt.plot(x, gemiddelde_waiting_times_per_percentiel)
-    plt.show()
+    return gemiddelde_response_ratios_per_percentiel, gemiddelde_waiting_times_per_percentiel
 
     
 def calculate_mean(scheduled):
